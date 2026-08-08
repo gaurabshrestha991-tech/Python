df68e25755e5917cd938ecf7ff3cdc6dee05c3a9
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-08-08 23:11:55 +0545

    Create string_indexing_slicing.py

---
+++ string_indexing_slicing.py
@@ -0,0 +1,13 @@
+text = "Hello, Python!"
+
+print(text[0])
+print(text[-1])
+print(text[-5])
+
+print(text[0:5])
+print(text[7:])
+print(text[:5])
+print(text[::-1])
+print(text[7:0:-1])
+print(text[::2])
+print(text[-7:-1])
