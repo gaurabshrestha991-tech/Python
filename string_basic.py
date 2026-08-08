e825cfbccd4e3cf18729a0ba67e222662fc196b6
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-08-08 23:08:22 +0545

    Implement basic string operations in string_basic.py
    Add string operations including concatenation, repetition, and length calculation.

---
+++ string_basic.py
@@ -0,0 +1,28 @@
+single = 'Hello'
+double = "world"
+multi_line = """This is
+a multi-line string"""
+
+print(single)
+print(double)
+print(multi_line)
+
+# string concatination and repetition
+greetings = "Hello" + "World"
+print(greetings)
+
+print("Ha" * 3)
+print("Hello\n" * 3)
+print("I am learning Python, " * 2)
+
+
+#string length
+single = "Hello"
+multi_line = """This is
+a multi-line string"""
+
+text = "Python c"
+
+print(len(text))
+print(len(single))
+print(len(multi_line))
