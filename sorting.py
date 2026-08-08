92f031d5d2ebef93f7bdb6ed30956c1081dd5398
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-08-08 22:49:12 +0545

    Add sorting examples for numbers and strings

---
+++ sorting.py
@@ -0,0 +1,18 @@
+numbers = [10, 20, 3, 45, 5, 6]
+
+sorted_numbers = sorted(numbers)
+
+print(sorted_numbers)
+
+## Sorting in reverse order
+numbers = [10, 20, 3, 45, 5, 6]
+
+sorted_numbers = sorted(numbers, reverse=True)
+
+print(sorted_numbers)
+
+#Sorting Strings
+
+names = ["ram", "hari", "kamal"]
+
+print(sorted(names))
