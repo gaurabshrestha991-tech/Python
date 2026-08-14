4062d5910ca8336409df6da1f25b216e56248a67
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-08-14 22:08:11 +0545

    Create check_key_length.py

---
+++ dictionary_sets/check_key_length.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Rahul",
+    "age": 19,
+    "school": "ABC School"
+}
+
+print("name" in student)      # True
+print("weight" in student)    # False
+
+print(len(student))
