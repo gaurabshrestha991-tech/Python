b4c6ad7233fe358d5502b3111ec7a83028802efc
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-08-30 20:04:46 +0545

    Create ceiling_floor.py

---
+++ ceiling_floor.py
@@ -0,0 +1,11 @@
+#       Program to implement celing and floor function
+
+import math
+
+num = float(input("Enter a number: "))
+
+ceiling = math.ceil(num)
+floor = math.floor(num)
+
+print("Ceiling value: ", ceiling)
+print("Floor value: ", floor)
