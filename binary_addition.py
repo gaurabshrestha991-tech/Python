daaac587945e68c8cbd0b7048bb6a5620937b616
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-08-30 20:30:06 +0545

    Implement binary integer addition program
    This program takes two binary numbers as input and outputs their binary sum.

---
+++ binary_addition.py
@@ -0,0 +1,13 @@
+  Program to implement binary integer addition
+
+a = (input("Enter first binary number: "))
+b = (input("Enter second binary number: "))
+
+num1 = int(a, 2)
+num2 = int(b, 2)
+
+result = num1 + num2
+
+binary_result = bin(result)[2:]
+
+print("Binary Addition =", binary_result)
