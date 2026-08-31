d47f6890ecf66ae339c54db9b8575716bcaef3f8
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-08-31 21:43:19 +0545

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+#       Bubble Sort
+
+def bubble_sort(arr):
+    n = len(arr)
+    
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+arr = list(map(int, input("Enter elements:").split()))
+
+bubble_sort(arr)
+
+print("Sorted array: ", arr)
